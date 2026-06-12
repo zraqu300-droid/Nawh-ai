ed1177db1a40c932c4ef5ac2381b9245739b0df7
zraqu300-droid <zraqu300@gmail.com>
2026-06-13 02:30:37 +0300

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; // استدعاء ملف الـ CSS العام
 
+// استدعاء موفر السياق الحقيقي لإدارة المظهر واللغة والأبعاد
+import { ThemeLanguageProvider } from './context/ThemeLanguageContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeLanguageProvider>
+      <App />
+    </ThemeLanguageProvider>
   </React.StrictMode>
 );
