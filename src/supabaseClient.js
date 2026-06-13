af710b47087d983c946e249573e2783d3441a230
zraqu300-droid <zraqu300@gmail.com>
2026-06-14 02:54:28 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-base-js'; // أو من @supabase/supabase-js حسب الحزمة المثبتة لديك
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wlbpqnbkbonbbuodmfcq.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsYnBxbmJrYm9uYmJ1b2RtZmNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzODExMjIsImV4cCI6MjA5Njk1NzEyMn0.SUgQ3j1o96rAvJjT9roCKPLllq-KkCSDCorq9cz2a1c';
