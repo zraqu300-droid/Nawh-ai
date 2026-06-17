738a38a42f9de270717f0ee2d23c17d8f92d95c8
zraqu300-droid <zraqu300@gmail.com>
2026-06-17 04:08:53 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import { Preferences } from '@capacitor/preferences';
 
 // تم تحديث الرابط والمفتاح بالبيانات الجديدة التي زودتني بها
-const supabaseUrl = 'https://bifewdnqarrzcdwybfxq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpZmV3ZG5xYXJyemNkd3liZnhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NTM0NjQsImV4cCI6MjA5NzEyOTQ2NH0.WB36qyVaISYYG7dIVNwDdtWR5kc_Fo-6jGEIWI4wjh0';
+const supabaseUrl = 'https://embbzefopkkvylcqobjm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtYmJ6ZWZvcGtrdnlsY3FvYmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NDMwNDIsImV4cCI6MjA5NzIxOTA0Mn0.madb-B0LF2HYwUl4q-DpbE5X88cVJaLtWJj1Idxq3rY';
 
 // 1️⃣ إعداد مخزن بيانات مخصص يتصل بذاكرة الموبايل الأصلية عبر كاباسيتور
 const capacitorAuthStorage = {
@@ -14,7 +14,7 @@ const capacitorAuthStorage = {
   setItem: async (key, value) => {
     await Preferences.set({ key, value });
   },
-  removeItem: async (key) => {
+  removeItem: async (key, value) => {
     await Preferences.remove({ key });
   },
 };
@@ -23,7 +23,7 @@ const capacitorAuthStorage = {
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: capacitorAuthStorage, // إجبار الحفظ في ذاكرة الجهاز وليس المتصفح الوهمي
-    autoRefreshToken: true,        // تحديث مفتاح الدخول تلقائياً لمنع خروج المستخدم مفاجئاً
+    autoRefreshToken: true,         // تحديث مفتاح الدخول تلقائياً لمنع خروج المستخدم مفاجئاً
     persistSession: true,          // استمرار الجلسة حتى بعد إغلاق التطبيق تماماً
     detectSessionInUrl: false      // إيقافها لأنك تعمل ببيئة تطبيق موبايل وليس ويب عادي
   },
